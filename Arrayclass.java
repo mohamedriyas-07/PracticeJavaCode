f64dc7ac4408b356d5217dc7f296b940ac526429
mohamedriyas-07 <mohamedriyasm012004@gmail.com>
2025-07-03 11:48:50 +0530

    Add files via upload

---
+++ Arrayclass.java
@@ -0,0 +1,26 @@
+package dear;
+import java.util.*;
+
+public class Arrayclass {
+
+	public static void main(String[] args) {
+		ArrayList<String> water=new ArrayList<String>();
+		water.add("Add");
+		water.add("subtract");
+		water.add("multiply");
+		water.add("divide");
+		water.set(0,"vijay");
+		water.remove(3);
+		water.clear();
+		System.out.println(water);
+		// TODO Auto-generated method stub
+		for(int i=0;i<water.size();i++) {
+			
+		}
+			
+		
+		
+
+	}
+
+}
